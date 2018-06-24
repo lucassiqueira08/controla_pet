24fe53be54b6667ce63849dfc534c9b8de8b9512
isaque015 <isaque015@outlook.com.br>
2018-06-24 18:15:48 -0300

    deploy

--- controla_pet/settings.py
+++ controla_pet/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['systemcontrolapet.herokuapp.com/']
+ALLOWED_HOSTS = ['systemcontrolapet.herokuapp.com']
 
 
 

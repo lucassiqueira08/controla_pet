492cc35e0d9145a4eafb5321bd5c74ca41d9f798
Lucas Araujo de Oliveira <lucas11200@hotmail.com>
2018-08-10 02:10:03 -0300

    upload das paginas - incompleto

---
+++ core/static/js/calendar/calendario2.js
@@ -0,0 +1,8 @@
+	$('.btn-canc-vis').on("click", function(){
+	$('.form').slideToggle();
+	$('.visualizar').slideToggle();
+	});
+		$('.btn-canc-edit').on("click", function(){
+	$('.visuaizar').slideToggle();
+	$('.form').slideToggle();
+	});

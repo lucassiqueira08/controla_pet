a29114c5bf0cae9f8c454b2717ac1a1ceb88609b
Lucas11200 <lucas11200@hotmail.com>
2018-09-01 20:12:40 -0300

    upload paginas ja trazendo o nome delas e o nome do usuario logado

--- core/views.py
+++ core/views.py
@@ -10,25 +10,22 @@ class BaseView(LoginRequiredMixin, View):
     redirect_field_name = 'redirect_to'
     login_url = '/usuario/login'
     permission_required = 'usuarios.Admin'
-
+    lista_titulos = MenuGrupo.objects.filter
 
 class ViewIndexBemVindo(BaseView):
 
     template = 'index_bemvindo.html'
     
     def get(self, request):
-        return render(request, self.template)
-
-
+        context = {
+            'usuario_nome': request.user.primeiro_nome.title()
+        }
+        return render(request, self.template, context)
+        
 class ViewIndex(BaseView):
 
     template = 'index.html'
 
     def get(self, request):
-
-        context = {
-            'menu': Menu.objects.all().order_by('ordem'),
-            'MenuGrupo': MenuGrupo.objects.all().order_by('ordem')
-        }
-        return render(request, self.template, context)
+        return render(request, self.template)
 

326b3ab2a8e5a8f3ef5a4a80298988b056c4a4d0
isaque015 <isaque015@outlook.com.br>
2018-09-01 21:37:27 -0300

    Criando logout

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,7 +5,7 @@ from usuarios.views import ViewEsqueceuSenha
 
 urlpatterns = [
     path('login/', login, {'template_name': 'login.html'}, name='login'),
-    path('logout/', logout, {'next_page': '/usuario/login/'}, name='logout'),
+    path('logout/', logout, {'next_page': 'login'}, name='logout'),
 
     path(
         'esqueceu_senha/', ViewEsqueceuSenha.as_view(), name='esqueceu_senha'

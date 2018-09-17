b4c33a003126a131fad27c6af1cac5133753320a
bruno1576 <brunosalve1@hotmail.com>
2018-09-16 21:48:29 -0300

    Alterações para o funcionamento da agenda

--- core/static/js/geral.js
+++ core/static/js/geral.js
@@ -78,3 +78,11 @@ FileInputBtn.addEventListener("change", function(){
 		fileInputSpan.innerHTML = "Nenhum arquivo selecionado...";
 	}
 });
+
+function Mudar(el){
+	var display = document.getElementById(el).style.display;
+	if(display == "none")
+		document.getElementById(el).style.display = 'block';
+	 else
+	 	document.getElementById(el).style.display ='none';
+}

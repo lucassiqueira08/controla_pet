c424c89647db5ab10ebb22172ffd9c455096e383
isaque015 <isaque015@outlook.com.br>
2018-09-26 19:46:48 -0300

    fixing bug save animal

--- cliente/views.py
+++ cliente/views.py
@@ -62,7 +62,7 @@ class ViewCadastrarAnimal(BaseView):
         animal.datanasc = datetime.strptime(datanasc, "%d/%m/%Y").strftime('%Y-%m-%d')
         animal.observacao = request.POST.get('observacao')
         animal.microchip = request.POST.get('microchip')
-        animal.cpf_cliente = request.POST.get('cpf')
+        animal.cpf_cliente = cliente
         animal.save()
 
         responde = Responde()
@@ -116,7 +116,7 @@ class ViewVisualizarAnimal(BaseView):
         animal.datanasc = datetime.strptime(datanasc, "%d/%m/%Y").strftime('%Y-%m-%d')
         animal.observacao = request.POST.get('obs')
         animal.microchip = request.POST.get('microchip')
-        animal.cpf_cliente = cpf_cliente  
+        animal.cpf_cliente = cpf_cliente
         if request.POST.get('button') == 'del':
             animal.delete()
         if request.POST.get('button')  == 'save':

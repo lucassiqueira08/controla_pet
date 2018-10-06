753f13e4a7d806f84e98ad3be99bad22c22f4d4d
lucassiqueira08 <lalvessiqueira8@gmail.com>
2018-10-06 16:55:09 -0300

    retornando dados do banco para o front em json

--- core/actions.py
+++ core/actions.py
@@ -1,34 +1,98 @@
 
 import datetime
+import json
 
 from django.http import HttpResponse
+from django.db import connection
 from django.core import serializers
+from django.core.serializers.json import DjangoJSONEncoder
 
 from servicos.models import Atendimento, FeitoPor, AtendimentoProcClinico, AtendimentoProcEstetico, StatusAtendimento
 
 
-
-def get_Atendimentos(request):
+def get_atendimentos(request):
     atendimento = Atendimento.objects.all()
     atendimento_json = serializers.serialize("json", atendimento)
     return HttpResponse(atendimento_json, content_type='application/json')
 
 
+def dictfetchall(cursor):
+    """
+        Retorna todas as linhas do cursor como um dicionario
+    """
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+
+
 def get_notificacao(request):
-    atendimentos = Atendimento.objects.prefetch_related((data_solicitacao__contains='2018-10').only('comissao'))
-    atendimento_json = serializers.serialize("json", atendimentos)
-    response = []
-    for atendimento in atendimentos:
-        feito_por = FeitoPor.objects.filter(id_atendimento=atendimento.pk)
-        feito_por_json = serializers.serialize("json", feito_por)
-        response.append(feito_por_json)
+    """
+    Função que retorna dados para a que a notificação seja exibida no frontend
 
+    param: request
+    return: json
 
-    # func = FeitoPor.objects.filter(id_atendimento=atendimento)
-    # func_json = serializers.serialize("json", func)
+    TODO: Melhorar a função para que não abra conexão com o banco de dados diretamente, e sim utilize o Django ORM
+    """
+    date_now = datetime.datetime.now()
+    year = date_now.strftime("%Y")
+    month = date_now.strftime("%m")
+    query_date = year + '-' + month
 
-    # proc_clinico = AtendimentoProcClinico.objects.all()
-    # proc_estetico = AtendimentoProcEstetico.objects.all()
-    # status = StatusAtendimento.objects.all()
+    try:
+        query = """
+            SELECT
+                ATE.id AS id,
+                TSA.nome AS status_atendimento,
+                ATE.data_solicitacao AS 'data/hora',
+                PE.especie AS especie,
+                PE.nome AS procedimento_estetico,
+                PC.nome AS procedimento_clinico,
+                CLI.nome AS dono,
+                CLI.cpf AS cpf_cliente,
+                U.primeiro_nome AS responsavel
+            FROM
+                ATENDIMENTO AS ATE
+                    LEFT JOIN
+                ATENDIMENTO_PROC_ESTETICO AS ATE_PE ON (ATE.id = ATE_PE.id_atendimento)
+                    LEFT JOIN
+                PROCEDIMENTO_ESTETICO AS PE ON (PE.id = ATE_PE.id_proc_estetico)
+                    LEFT JOIN
+                CLIENTE AS CLI ON (ATE.cpf_cliente = CLI.cpf)
+                    LEFT JOIN
+                ATENDIMENTO_PROC_CLINICO AS ATE_CL ON (ATE.id = ATE_CL.id_atendimento)
+                    LEFT JOIN
+                PROCEDIMENTO_CLINICO AS PC ON (PC.id = ATE_CL.id_proc_clinico)
+                    LEFT JOIN
+                FEITO_POR AS FP ON (FP.id_atendimento = ATE.id)
+                    LEFT JOIN
+                FUNCIONARIO AS FUN ON (FUN.user_ptr_id = FP.id_funcionario)
+                    LEFT JOIN
+                USER AS U ON (U.id = FUN.user_ptr_id)
+                    LEFT JOIN
+                STATUS_ATENDIMENTO AS ST ON (ATE.id = ST.id_atendimento)
+                    LEFT JOIN
+                TIPO_STATUS_ATENDIMENTO AS TSA ON (TSA.id = ST.id_status);
+            WHERE
+                ATE.data_solicitacao = %s
+        """ % query_date
 
-    return HttpResponse(atendimento_json, content_type='application/json')
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            row = dictfetchall(cursor)
+
+        atendimentos = row
+
+        context = json.dumps(
+            atendimentos,
+            sort_keys=True,
+            indent=1,
+            cls=DjangoJSONEncoder
+        )
+
+    except Exception:
+        context = {}
+
+    return HttpResponse(json.dumps(context), content_type='application/json')
--- core/static/js/base.js
+++ core/static/js/base.js
@@ -1,4 +1,18 @@
 $.get('/get_notificacao', function(data){
-  console.log(data)
+
+  var dataJson = JSON.parse(data)
+
+  var notificacao = $('#notificacoes')
+  for (var item in dataJson) {
+    var id = $('<p/>').text(dataJson[item]['id'])
+    var hora = $('<p/>').text(dataJson[item]['data/hora'])
+    var dono = $('<p/>').text(dataJson[item]['dono'])
+    var cpf_cliente = $('<p/>').text(dataJson[item]['cpf_cliente'])
+    var procedimento_clinico = $('<p/>').text(dataJson[item]['procedimento_clinico'])
+    var procedimento_estetico = $('<p/>').text(dataJson[item]['procedimento_estetico'])
+    var responsavel = $('<p/>').text(dataJson[item]['responsavel'])
+    var status_atendimento = $('<p/>').text(dataJson[item]['status_atendimento'])
+    notificacao.append(hora, dono, procedimento_clinico, procedimento_estetico,responsavel,status_atendimento)
+  }
+  console.log(dataJson)
 })
-console.log('data')

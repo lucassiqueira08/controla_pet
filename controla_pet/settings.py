ca1311c3283f07f9b120b9d4b4244fa84e9ed9d0
lucassiqueira08 <lalvessiqueira8@gmail.com>
2018-10-21 00:52:28 -0300

    correcao allowed hosts

--- controla_pet/settings.py
+++ controla_pet/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
 
 

0ffc6fde29c1903241e9a21531b0a44b1e65f370
bruno1576 <brunosalve1@hotmail.com>
2018-11-10 22:35:42 -0200

    Finalizado e corrigo

--- cliente/static/js/cadastro_cliente.js
+++ cliente/static/js/cadastro_cliente.js
@@ -101,14 +101,13 @@ $('#animal_botao').on('click', function (e) {
           },
       })
 })
-console.log('teste')
+
 $('#achar_animal').click(function(){
 
   cpf_cliente = $('#cpf_cliente').val()
   nome_animal = $('#nome_animal').val()
 
   $.getJSON('/cliente/get_ficha_animal'+ '/' + cpf_cliente +  '/' + nome_animal , function (data) {
-    console.log(data) 
     $('#nome').val(data[0].fields.nome)
     $('#raca').val(data[0].fields.raca)
     $('#dono').val(data[1].fields.nome)
